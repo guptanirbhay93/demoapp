530540d50fb9ac7beecde109ab52713e458bc122
Nirbhay Gupta <guptanirbhay93@gmail.com>
2018-06-27 21:18:22 +0530

    Minor fix

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,23 +5,25 @@ const dbService = require("../db/services");
 
 passport.use(
   new Strategy((token, cb) => {
-    dbService
-      .findSession(token)
-      .then(
-        session =>
-          dbService
-            .getUserByEmail(session.email)
-            .then(
-              user =>
-                cb(
-                  null,
-                  { token, ...user },
-                  { token, id: user.id, email: user.email }
-                ),
-              err => cb(null, false)
-            ),
-        err => cb(err, null)
-      );
+    dbService.findSession(token).then(
+      session =>
+        dbService.getUserByEmail(session.email).then(
+          user => {
+            if (user) {
+              cb(
+                null,
+                { token, ...user },
+                { token, id: user.id, email: user.email }
+              );
+            } else {
+              cb(null, { token }, { token });
+            }
+          },
+
+          err => cb(null, false)
+        ),
+      err => cb(err, null)
+    );
   })
 );
 
